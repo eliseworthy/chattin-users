33028c51e2bc4b1d29f6d8f5bee250d3b5db6bfa
Elise Worthy <eworthy@bluebox.net>
2012-06-03 15:31:52 -0400

    all client-side user methods created and tested

--- client.rb
+++ client.rb
@@ -7,7 +7,7 @@ class User
   class << self; attr_accessor :base_uri end
   
   def self.find_by_name(name)
-    response = Thyphoeus::Request.get(
+    response = Typhoeus::Request.get(
                "#{base_uri}/api/v1/users/#{name}"
                )
     if response.code == 200
@@ -17,5 +17,35 @@ class User
     else 
       raise response.body
     end
+  end
+  
+  def self.create(attributes)
+    response = Typhoeus::Request.post(
+      "#{base_uri}/api/v1/users",
+      body: attributes.to_json
+      )
+    if response.code == 200
+      JSON.parse(response.body)["user"]
+    else
+      raise response.body
+    end
+  end  
+  
+  def self.update(name, attributes)
+    response = Typhoeus::Request.put(
+      "#{base_uri}/api/v1/users/#{name}",
+      body: attributes.to_json
+      )
+    if response.code == 200
+      JSON.parse(response.body)["user"]
+    else
+      raise response.body
+    end
+  end
+  
+  def self.destroy(name)
+    Typhoeus::Request.delete(
+      "#{base_uri}/api/v1/users/#{name}"
+      ).code == 200
   end
 end        
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -9,12 +9,19 @@ describe "client" do
 
     User.destroy("elise")
     User.destroy("bookis")
+    User.destroy("squilio")
 
     User.create(
-      :name => "elise",
-      :email => "elise@example.com",
-      :password => "password",
-      :bio => "girloo")
+      name: "elise",
+      email: "elise@example.com",
+      password: "password",
+      bio: "girloo")
+      
+    User.create(
+      name: "squilio",
+      email: "no",
+      password: "bushytail",
+      bio: "just a squirrel")  
   end
   
   it "should get a user" do 
@@ -27,4 +34,28 @@ describe "client" do
   it "should return nil for a user not found" do
     User.find_by_name("bookis").should be_nil
   end
+  
+  it "should create a user" do
+    user = User.create(
+      name: "bookis",
+      email: "bookis@example.com",
+      password: "password"
+    )
+    
+    user["name"].should == "bookis"
+    user["email"].should == "bookis@example.com"
+    User.find_by_name("bookis").should == user
+  end
+  
+  it "should update a user" do
+    user = User.update("elise", {bio: "girly girloo"})
+    user["name"].should == "elise"
+    user["bio"].should == "girly girloo"
+    User.find_by_name("elise").should == user
+  end
+  
+  it "should destroy a user" do
+    User.destroy("squilio").should == true
+    User.find_by_name("squilio").should be_nil
+  end  
 end
